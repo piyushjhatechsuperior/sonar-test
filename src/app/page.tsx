884caff0413d46277964493a3b6c926fedab2583
piyushjhatechsuperior <piyush.jhatechsuperiors.com>
2026-01-15 10:08:55 +0530

    unused function added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,10 @@ import { useEffect } from "react";
 export default function Home() {
   console.log("unused ads", adsfs);
 
+  function add(a: number, b: number): number {
+    return a + b;
+  }
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
